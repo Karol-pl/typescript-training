d60b2d94be55551e0bb7ec3a756a1f1cfe8e78fd
Karol Homziuk <karolhomziuk@gmail.com>
2021-10-18 12:38:17 +0200

    add js file

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,30 @@
 "use strict";
 var Department = /** @class */ (function () {
-    function Department(n) {
-        this.name = n;
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        // private id: string;
+        // private name: string;
+        this.employees = [];
+        // this.id = id;
+        // this.name = n;
     }
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.id + ": " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
-var accounting = new Department("Accounting");
-console.log(accounting);
+var accounting = new Department("d1", "Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Karol");
+accounting.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: "Dummy", describe: accounting.describe };
+// accountingCopy.describe();
