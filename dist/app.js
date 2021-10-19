b70c711c28edd017fa97baee664088523cab80b4
Karol Homziuk <karolhomziuk@gmail.com>
2021-10-19 20:27:34 +0200

    index properties

--- dist/app.js
+++ dist/app.js
@@ -63,4 +63,10 @@ function moveAnimal(animal) {
 moveAnimal({ type: "bird", flyingSpeed: 10 });
 // const userInputElement = <HTMLInputElement>document.getElementById("user-input")!;
 var userInputElement = document.getElementById("user-input");
-userInputElement.value = "Hi there!";
+if (userInputElement) {
+    userInputElement.value = "Hi there";
+}
+var errorBag = {
+    email: "Not a valid email",
+    username: "Must start with a character",
+};
--- src/app.ts
+++ src/app.ts
@@ -98,8 +98,18 @@ function moveAnimal(animal: Animal) {
 moveAnimal({ type: "bird", flyingSpeed: 10 });
 
 // const userInputElement = <HTMLInputElement>document.getElementById("user-input")!;
-const userInputElement = document.getElementById(
-  "user-input"
-)! as HTMLInputElement;
+const userInputElement = document.getElementById("user-input");
 
-userInputElement.value = "Hi there!";
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there";
+}
+
+interface ErrorContainer {
+  // {email: 'Not a valid email', username: 'Must start with a character'}
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: "Not a valid email",
+  username: "Must start with a character",
+};
