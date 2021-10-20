da360d175360793bf08480f826b9ef7878d575b8
Karol Homziuk <karolhomziuk@gmail.com>
2021-10-20 17:09:11 +0200

    creating a generic function

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,15 @@
+"use strict";
 // const names: Array<string> = [];
-
 // const promise: Promise<number> = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     resolve("This is done");
 //   }, 2000);
 // });
-
 // promise.then((data) => {
 //   data.split(" ");
 // });
-
-function merge<T, U>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
 }
-
 const mergedObj = merge({ name: "Karol", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj.age);
