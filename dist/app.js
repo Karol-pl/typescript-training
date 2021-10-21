c818f01a91e70dbe18af84002a66837ee9fb7bad
Karol Homziuk <karolhomziuk@gmail.com>
2021-10-21 11:10:43 +0200

    more useful decorators

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(constructor) {
-    console.log("Logging...");
-    console.log(constructor);
+function Logger(logString) {
+    return function (constructor) {
+        console.log("Logging...");
+        console.log(constructor);
+    };
 }
+function WithTemplate(template, hookId) {
+    return function (constructor) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1").textContent = p.name;
+        }
+    };
+}
+// @Logger("LOGGING - PERSON")
 let Person = class Person {
     constructor() {
         this.name = "Karol";
@@ -16,7 +29,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger
+    WithTemplate("<h1>My person object</h1>", "app")
 ], Person);
 const pers = new Person();
 console.log(pers);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
-function Logger(constructor: Function) {
-  console.log("Logging...");
-  console.log(constructor);
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log("Logging...");
+    console.log(constructor);
+  };
 }
 
-@Logger
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My person object</h1>", "app")
 class Person {
   name = "Karol";
 
